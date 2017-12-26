8cba2632f1b37e7e708257dcfdbbfbf25d75a9b4
subrahmanyam.b <subrahmanyam.b@gofrugal.com>
2017-12-26 17:04:30 +0530

    changes are done to auto-redirection to https

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 
 app.listen(process.env.PORT || 8080);
+
+// server.js
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+
+app.use(forceSSL());
