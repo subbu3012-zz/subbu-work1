b84544a056aba7aa3dbb6831ddec1e8b8404b91b
subrahmanyam.b <subrahmanyam.b@gofrugal.com>
2017-12-27 16:39:48 +0530

    changes are done to remove unwanted dependencies in vendor

--- client_src/src/vendor.ts
+++ client_src/src/vendor.ts
@@ -1,15 +1,15 @@
-// Angular
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
-import '@angular/core';
-import '@angular/common';
-import '@angular/http';
-import '@angular/router';
-// RxJS
-import 'rxjs';
-// Other vendors for example jQuery, Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
+// // Angular
+// import '@angular/platform-browser';
+// import '@angular/platform-browser-dynamic';
+// import '@angular/core';
+// import '@angular/common';
+// import '@angular/http';
+// import '@angular/router';
+// // RxJS
+// import 'rxjs';
+// // Other vendors for example jQuery, Lodash or Bootstrap
+// // You can import js, ts, css, sass, ...
 
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'jquery/dist/jquery.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css'
